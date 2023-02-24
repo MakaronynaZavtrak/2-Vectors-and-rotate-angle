93cb7b8f8f0e2def7ca05ec4da8a72d028b5331f
ElectricGtr <114348027+MakaronynaZavtrak@users.noreply.github.com>
2023-02-24 12:11:43 +0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,8 @@
+#pragma once
+double findLength(int a[]);
+int scalMultiplication(int a[], int b[]);
+double getAngle(int vector1[],int vector2[]);
+int* findNormalVector(int a[], int b[]);
+int mixedMultiplication(int a[], int b[], int c[]);
+double* getRotatedVector(int arr[],double rotateAngle);
+void printVector(double arr[]);
